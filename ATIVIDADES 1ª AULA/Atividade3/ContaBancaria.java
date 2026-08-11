ba3e3ff87e0e6e1804ef3c459ec2e2aa58fbfd15
Ferdinand Nilo Trebien <ferdinadtrebien@gmail.com>
2026-08-10 22:11:50 -0300

    Add ContaBancaria class with basic banking operations

---
+++ ATIVIDADES 1ª AULA/Atividade3/ContaBancaria.java
@@ -0,0 +1,33 @@
+public class ContaBancaria {
+    String titular;
+    double saldo;
+
+    void depositar(double valor) {
+        saldo += valor;
+        System.out.println("Depósito realizado");
+    }
+
+    void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            System.out.println("Saque realizado");
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    void exibirSaldo() {
+        System.out.println("Titular: " + titular);
+        System.out.println("Saldo: R$ " + saldo);
+    }
+
+    void transferir(ContaBancaria destino, double valor) {
+        if (valor <= saldo) {
+            sacar(valor);
+            destino.depositar(valor);
+            System.out.println("Transferência realizada");
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+}
