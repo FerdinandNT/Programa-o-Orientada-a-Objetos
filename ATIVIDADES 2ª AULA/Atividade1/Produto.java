b6f5fd6413369c4136ac16f4bd1b4c5117ba6d4b
Ferdinand Nilo Trebien <ferdinadtrebien@gmail.com>
2026-08-17 20:54:46 -0300

    Add Produto class with name and price attributes

---
+++ ATIVIDADES 2ª AULA/Atividade1/Produto.java
@@ -0,0 +1,11 @@
+package Atividade1;
+
+public class Produto {
+    String nome;
+    double preco;
+
+    public Produto(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
