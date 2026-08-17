12e4a04c8527bece78c2e70549d2adeaa84fe7b5
Ferdinand Nilo Trebien <ferdinadtrebien@gmail.com>
2026-08-17 20:52:28 -0300

    Create Cliente.java

---
+++ ATIVIDADES 2ª AULA/Atividade1/Cliente.java
@@ -0,0 +1,11 @@
+package Atividade1;
+
+public class Cliente{
+    String nome;
+    String email;
+
+    public Cliente(String nome, String email){
+        this.nome = nome;
+        this.email = email;
+    }
+}
