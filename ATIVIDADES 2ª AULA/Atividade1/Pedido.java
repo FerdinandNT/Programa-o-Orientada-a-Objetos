acaf8f35a12a3aaa4b7cac325de771c6e2bc77d3
Ferdinand Nilo Trebien <ferdinadtrebien@gmail.com>
2026-08-17 20:53:15 -0300

    Add Pedido class for order management

---
+++ ATIVIDADES 2ª AULA/Atividade1/Pedido.java
@@ -0,0 +1,23 @@
+package Atividade1;
+
+public class Pedido {
+    Cliente cliente;
+    Produto produto;
+    int quantidade;
+
+    public Pedido(Cliente cliente, Produto produto, int quantidade){
+        this.cliente = cliente;
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+    
+    void resumo(){
+        double valorTotal = produto.preco * quantidade;
+
+        System.out.println("Cliente: " + cliente.nome);
+        System.out.println("Produto: " + produto.nome);
+        System.out.println("Quantidade: " + quantidade);
+        System.out.println("Valor Total: R$" + valorTotal);
+
+    }
+}
