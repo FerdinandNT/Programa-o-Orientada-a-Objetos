19c8ef1fcd2f5297b501420a76ffbd843bf1d616
Ferdinand Nilo Trebien <ferdinadtrebien@gmail.com>
2026-08-17 21:11:40 -0300

    Create Autor.java

---
+++ ATIVIDADES 2ª AULA/Atividade3/Autor.java
@@ -0,0 +1,11 @@
+package Atividade3;
+
+public class Autor {
+    String nome;
+    String nacionalidade;
+
+    public Autor(String nome, String nacionalidade) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+    }
+}
