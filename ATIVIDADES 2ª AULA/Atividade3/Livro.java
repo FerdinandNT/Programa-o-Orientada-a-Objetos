f8b1ba14d03b76b2cc736993244feed32d636ad6
Ferdinand Nilo Trebien <ferdinadtrebien@gmail.com>
2026-08-17 21:12:12 -0300

    Add Livro class with basic functionalities

---
+++ ATIVIDADES 2ª AULA/Atividade3/Livro.java
@@ -0,0 +1,30 @@
+package Atividade3;
+
+public class Livro {
+    String titulo;
+    Autor autor;
+    int ano;
+    boolean emprestado;
+
+    public Livro(String titulo, Autor autor, int ano, boolean emprestado) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.emprestado = emprestado;
+    }
+
+    void exibirFicha() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor.nome);
+        System.out.println("Ano: " + ano);
+    }
+
+    void emprestar() {
+        if (emprestado) {
+            System.out.println("Livro já está emprestado!");
+        } else {
+            emprestado = true;
+            System.out.println("Livro emprestado com sucesso!");
+        }
+    }
+}
